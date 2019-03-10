58604da7475ff4a592f89a56c6151b2a6e244e9f
healchow <heal.chow@gmail.com>
2019-03-10 11:08:10 +0800

    Create post03-Constant.java

---
+++ java-basic/src/main/java/com/healchow/blog/post03/Constant.java
@@ -0,0 +1,27 @@
+package com.healchow.blog.post03;
+
+/**
+ * 这是关于常量的测试类
+ * 常量: 在程序执行的过程中, 值不可以发生改变的量
+ *
+ * @author Heal Chow
+ * @date: 2019/02/22 23:48
+ */
+
+public class Constant {
+    public static void main(String[] args) {
+        // 字符串常量
+        System.out.println("HelloWorld");
+        // 整数常量
+        System.out.println(12);
+        System.out.println(-23);
+        // 小数常量
+        System.out.println(12.34);
+        // 字符常量
+        System.out.println('a');
+        System.out.println('0');
+        // 布尔常量
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
