0b50a548d1a732dfdf8008056f462b523849604a
healchow <heal.chow@gmail.com>
2019-03-10 11:00:46 +0800

    Create post02-HelloWorld.java

---
+++ java-basic/src/main/java/com/healchow/blog/post02/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.healchow.blog.post02;
+
+/**
+ * 这是我的第一个Java程序-HelloWorld
+ *
+ * @author Heal Chow
+ * @date 2019/02/21 23:38
+ */
+public class HelloWorld {
+    /*
+     * 这是main方法, 是程序的入口方法, 所有代码的执行都是从main方法开始的
+     */
+    public static void main(String[] args) {
+        // 这是输出语句
+        System.out.println("Hello World!");
+    }
+}
